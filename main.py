731e1db4af175f4ebf9fed4dad3104d10dc6a0e0
Swapnil Prabhu <swapnilspra@gmail.com>
2018-03-27 12:08:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Hello, World!!! welcome to MWC service testing.'
 
 if __name__ == '__main__':
   app.run()
