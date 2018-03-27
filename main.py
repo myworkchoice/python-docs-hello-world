a26fc87a3ac87f6cbd9aa3b51db23fd21a2e15e8
Swapnil Prabhu <swapnilspra@gmail.com>
2018-03-27 14:02:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,9 @@
-#!flask/bin/python
-from flask import Flask, jsonify
-from flask import make_response
-from flask import request
-import json
-
+from flask import Flask
 app = Flask(__name__)
 
-shift = [
-    {
-        'id': '5ab5c822ec51f60988318e17',
-        'title': 'Coffe Maker',
-        'startdate': '20180326',
-        'Active': False
-    },
-    {
-        'id': '5ab5c822ec51f60988319e17',
-        'title': 'Marketing Officer',
-        'startdate': '20180329',
-        'Active': True
-    }
-]
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.errorhandler(500)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 500)
-
-@app.route('/api/v1.0/shift', methods=['GET'])
-def get_shifts():
-    input = request.args.get('shiftin')
-    output = json.loads(input)
-
-    if 'firstName' in output:
-        shiftid= output['firstName']
-        return jsonify(
-        {'firstName': shiftid})
-
-    else:
-        return jsonify(
-            {'Shift':output})
-        print(shift)
-        print(shiftid)
-
-
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
