1d60b300a0a76f4091dd2ce4dfa9c4cb4c65e2b7
Swapnil Prabhu <swapnilspra@gmail.com>
2018-03-27 14:23:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 from flask import Flask
-from flask import make_response
-from flask import request
-import json
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  # Set up the parameters we want to pass to the API.
-  # This is the latitude and longitude of New York City.
-  parameters = {"lat": 40.71, "lon": -74}
-
-  # Make a get request with the parameters.
-  #response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
-
-  # Print the content of the response (the data the server returned)
-  #print(response.content)
-
-  # This gets the same data as the command above
-  response = requests.get("http://api.open-notify.org/iss-pass.json?lat=40.71&lon=-74")
-  return  (response.content)
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run()
