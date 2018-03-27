69b1e3c20336fce97c5b819f68be578aa1def093
Swapnil Prabhu <swapnilspra@gmail.com>
2018-03-27 14:43:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 def get_shifts():
     return 'Hello, World!!!!!!!'
 
+@app.route("/proxy-example")
+def proxy_example():
+    r = requests.get("http://example.com/other-endpoint")
+    return Response(
+        r.text
+        status=r.status_code,
+        content_type=r.headers['content-type'],
+    )
 
 if __name__ == '__main__':
     app.run()
