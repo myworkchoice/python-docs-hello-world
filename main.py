72a9e280a2529da7a48b52319bcaa1b7ac593d6f
Swapnil Prabhu <swapnilspra@gmail.com>
2018-03-27 14:31:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,6 @@ import json
 
 app = Flask(__name__)
 
-shift = [
-    {
-        'id': '5ab5c822ec51f60988318e17',
-        'title': 'Coffe Maker',
-        'startdate': '20180326',
-        'Active': False
-    },
-    {
-        'id': '5ab5c822ec51f60988319e17',
-        'title': 'Marketing Officer',
-        'startdate': '20180329',
-        'Active': True
-    }
-]
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.errorhandler(500)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 500)
-
 @app.route('/api/v1.0/shift')
 def get_shifts():
     input = request.args.get('shiftin')
@@ -44,7 +20,6 @@ def get_shifts():
             {'Shift':output})
         print(shift)
         print(shiftid)
-
 
 
 if __name__ == '__main__':
