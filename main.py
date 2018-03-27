f56cdd81465feb5f59fc534aa82e1d88af580730
Swapnil Prabhu <swapnilspra@gmail.com>
2018-03-27 12:36:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-from flask import Flask
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import make_response
+from flask import request
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!!! welcome to MWC service testing.'
+shift = [
+    {
+        'id': '5ab5c822ec51f60988318e17',
+        'title': 'Coffe Maker',
+        'startdate': '20180326',
+        'Active': False
+    },
+    {
+        'id': '5ab5c822ec51f60988319e17',
+        'title': 'Marketing Officer',
+        'startdate': '20180329',
+        'Active': True
+    }
+]
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(500)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 500)
+
+@app.route('/api/v1.0/shift', methods=['GET'])
+def get_shifts():
+    input = request.args.get('shiftin')
+    output = json.loads(input)
+
+    if 'firstName' in output:
+        shiftid= output['firstName']
+        return jsonify(
+        {'firstName': shiftid})
+
+    else:
+        return jsonify(
+            {'Shift':output})
+        print(shift)
+        print(shiftid)
+
+
 
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
